8f385974802494e3face28df733fa289c7a17907
Patrik Cyprian <pcyprian@dhcp-10-40-4-252.brq.redhat.com>
2015-12-10 22:50:53 +0100

    domain heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -12,6 +12,7 @@ Rails.application.configure do
   config.action_mailer.smtp_settings = {
   :address   => "smtp.mandrillapp.com",
   :port      => 587,
+  :domain    => 'heroku.com',
   :user_name => ENV["MANDRILL_USERNAME"],
   :password  => ENV["MANDRILL_API_KEY"]
 }
